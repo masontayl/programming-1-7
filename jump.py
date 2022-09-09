115fc82e8ccaab1074641fcb52c1f101bea43131
masontayl <113059001+masontayl@users.noreply.github.com>
2022-09-09 19:20:25 +0000

    update

---
+++ jump.py
@@ -0,0 +1,21 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    move()
+    move()
+move()
+turn_left()
+move()
+move()
+move()
+move()
+turn_left()
+turn_left()
+turn_left()
+move()
+    pass
+
+
+    run_karel_program()
---
+++ prog52A.py
@@ -0,0 +1,11 @@
+
+length = int(intput("please enter length")
+width = int("please enter width: ")
+area = length * width
+perim = (2 * length) + (2 * width)
+
+print("the length is",length)
+print("the width is", width)
+
+print("\nthe area is",area)
+print("the perimeter is", perim
